f7caea8cc8af7ed6aa70280fdce65c3a81c3197d
daleclark <34519618+daleclark@users.noreply.github.com>
2019-03-04 12:47:41 -0600

    Initial commit

---
+++ src/Cmotor3958.cpp
@@ -0,0 +1,45 @@
+/* Implement PID control of TETRIX motors equipped with encoders using the PRIZM controller.
+ * The recommended DC motor is the TETRIX TorqueNADO - part number 44260.
+ * This example demonstrates how to use position targetting functions to accurately
+ * position a DC motor + encoder to an encoder count position and hold the position
+ * in a servo like mode. For more detailed information on using the PRIZM library functions,
+ * Please see the Arduino Library functions grid in the TETRIX PRIZM Programmer's guide 
+ * which can be viewed and downloaded at www.TETRIXRobotics.com
+ */
+
+
+#include <PRIZM.h>    // Include PRIZM Library
+
+PRIZM prizm;          // Instantiate an object named prizm
+
+
+void waitForMotors() {
+  while (true) {
+    if (prizm.readMotorBusy(1) || prizm.readMotorBusy(2)) {
+      break;
+    }
+  }
+  prizm.resetEncoders();
+}
+
+void setup() {
+
+  prizm.PrizmBegin(); // Initiates the PRIZM controller - must be called in the setup of each PRIZM sketch
+  
+
+}
+
+void loop() {
+
+  prizm.setMotorTargets(360,3958,360,-3958);   // Spin DC motors 1 and 2 at a constant 360 degrees per second and stop when encoder count reaches 1440 and -1440 respectively.
+                                               // For TETRIX TorqueNADO encoders, 1440 = 1 motor shaft revolution. (1440 / 4 = 360)
+
+  waitForMotors();
+
+  prizm.setMotorTargets(360,1979,360,-1979);         // Spin DC motors 1 and 2 at 360 degrees per second back to encoder position "0".   
+
+  delay(8000);    // wait here for 2 seconds, and repeat 
+
+  prizm.resetEncoders();
+                                      
+}
